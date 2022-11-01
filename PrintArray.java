57c52e8aecdf20abea1e67e945f1ada69b7b8e45
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-02 00:11:58 +0530

    Added: Program to print all element of array

---
+++ PrintArray.java
@@ -0,0 +1,10 @@
+package com.bridgelab.day3.array;
+
+public class PrintArray {
+	public static void main(String[] args) {
+	int array[] = { 10, 20, 30, 40, 50 };
+	for (int i = 0; i <array.length; i++) 
+		System.out.println("element at " +i+ " is " +array[i]);
+	}
+
+}
